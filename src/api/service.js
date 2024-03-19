c3afcd92bd6ea44b8b870a0ea78b2880a0e6c375
steniosousa <steniosousaf@gmail.com>
2024-03-19 13:34:21 -0300

    return backend

--- src/api/service.js
+++ src/api/service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'https://intermedium-connect-api-dev-dbcq.4.us-1.fl0.io'
+  baseURL:'https://intermedium-4d6b8dadff06.herokuapp.com'
+  // baseURL: 'https://intermedium-connect-api-dev-dbcq.4.us-1.fl0.io'
 
 });
 export default Api;
