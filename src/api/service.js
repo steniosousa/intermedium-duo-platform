a1c7950fb73450a0b4052adb09d3b7319ee399c5
steniosousa <steniosousaf@gmail.com>
2024-10-11 00:09:07 -0300

    remove .env

--- src/api/service.js
+++ src/api/service.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'https://intermedium-connect-api-production.up.railway.app'
-  // baseURL:'http://localhost:3333/'
+  baseURL: process.env.API_URL
 });
 export default Api;
